05e19fe0dd06a28e44cec8635e528b1b8394bca2
TarekNeGm <60225477+TarekNeGm@users.noreply.github.com>
2025-02-05 15:06:33 -0800

    Unit Of Work Is Done

---
+++ Interfaces/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using Interfaces.Repository;
+using System.Data.Common;
+
+namespace Interfaces.UnitOfWork
+{
+    public interface IUnitOfWork
+    {
+        public Task<int> Complete();
+        public void Dispose();
+        public Task<int> ExecuteSqlRawAsync(string sql, params object[] parameters);
+        public DbCommand CreateDbCommand();
+        public Task OpenConnectionAsync();
+        public Task CloseConnectionAsync();
+        public IRepository<TEntity, L> Repository<TEntity, L>() where TEntity : class where L : struct;
+    }
+}
---
+++ Services/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,61 @@
+﻿using Infrastructure.Context;
+using Interfaces.Repository;
+using Interfaces.UnitOfWork;
+using Microsoft.EntityFrameworkCore;
+using Services.Repository;
+using System.Collections;
+using System.Data;
+using System.Data.Common;
+
+namespace Services.UnitOfWork
+{
+    public class UnitOfWork(AppDbContext context) : IUnitOfWork
+    {
+        private readonly AppDbContext _context;
+        private Hashtable _repositories;
+
+        public async Task<int> Complete() => await _context.SaveChangesAsync();
+        public void Dispose() => _context.Dispose();
+        public async Task<int> ExecuteSqlRawAsync(string sql, params object[] parameters)
+        {
+            return await _context.Database.ExecuteSqlRawAsync(sql, parameters);
+        }
+        public DbCommand CreateDbCommand()
+        {
+            var connection = _context.Database.GetDbConnection();
+
+            var command = connection.CreateCommand();
+
+            return command;
+        }
+        public async Task OpenConnectionAsync()
+        {
+            if (_context.Database.GetDbConnection().State != ConnectionState.Open)
+            {
+                await _context.Database.OpenConnectionAsync();
+            }
+        }
+        public async Task CloseConnectionAsync()
+        {
+            if (_context.Database.GetDbConnection().State != ConnectionState.Closed)
+            {
+                await _context.Database.CloseConnectionAsync();
+            }
+        }
+        public IRepository<TEntity, T> Repository<TEntity, T>() where TEntity : class where T : struct
+        {
+            _repositories ??= [];
+
+            var type = typeof(TEntity).Name;
+
+            if (!_repositories.ContainsKey(type))
+            {
+                var repository = new RepositoryService<TEntity, T>(_context);
+
+                _repositories.Add(type, repository);
+            }
+
+            return (IRepository<TEntity, T>)_repositories[type]!;
+        }
+    }
+}
